4dfcb38682607ddcfb6d6e3c7d02de2a8e9ec3b6
liuy0421 <yuelin@umd.edu>
2020-11-02 12:34:47 -0500

    misc.

--- ColumnSelect.m
+++ ColumnSelect.m
@@ -4,15 +4,15 @@
 function [C] = ColumnSelect(A,c,k)
     
     % selecting columns of V from top k singular values
-    [U,S,V] = svd(A, 'econ');
-    Vk = V(:,1:k)
+    [~,~,V] = svd(A, 'econ');
+    Vk = V(:,1:k);
     p = (1/k).*sum(Vk.^2,2);
     
     % keeping column j with probability min{1,ptemp_j}
     ptemp = c.*p;
-    %rng('default');
     eta = rand(size(ptemp));
-    diff = ptemp-eta
-    ind = find(diff > 0)
+    diff = ptemp-eta;
+    ind = find(diff > 0);
+    C = A(:,ind);
     
 end
